909ce59cca0d8888f7045217e7ef488f11d9cfed
Roahn <109647650+rohanlearn@users.noreply.github.com>
2023-02-23 10:58:07 +0530

    Delete Pics-Extractor directory

--- Pics-Extractor/extractor/tet.py
+++
@@ -1,8 +0,0 @@
-from django.http import FileResponse
-
-def download_file(request):
-    file_path = '/path/to/file.txt'
-    response = FileResponse(open(file_path, 'rb'))
-    response['Content-Disposition'] = 'attachment; filename="file.txt"'
-    return response
-
